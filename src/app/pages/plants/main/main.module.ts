d2a2a016c524afa64677c8d9ad164f362d4b9110
bengodsan <47090175+bengodsan@users.noreply.github.com>
2020-07-23 22:24:44 +0200

    Bennie/layout (#8)
    * Creating basic layout
    
    * generating components
    
    * moving plant list
    
    * Fixing prettier

--- src/app/pages/plants/main/main.module.ts
+++ src/app/pages/plants/main/main.module.ts
@@ -4,6 +4,9 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 // Components
 import { LayoutComponent } from './parent/layout/layout.component';
+import { PlantDetailComponent } from './parent/plant-detail/plant-detail.component';
+import { PlantFilterComponent } from './parent/plant-filter/plant-filter.component';
+import { PlantMasonryGridComponent } from './parent/plant-masonry-grid/plant-masonry-grid.component';
 // Third Party
 import { NgxMasonryModule } from 'ngx-masonry';
 import { PlantCardComponent } from './child/plant-card/plant-card.component';
@@ -16,7 +19,13 @@ export const ROUTES: Routes = [
 ];
 
 @NgModule({
-  declarations: [LayoutComponent, PlantCardComponent],
+  declarations: [
+    LayoutComponent,
+    PlantCardComponent,
+    PlantDetailComponent,
+    PlantFilterComponent,
+    PlantMasonryGridComponent,
+  ],
   imports: [CommonModule, RouterModule.forChild(ROUTES), NgxMasonryModule],
 })
 export class MainModule {}
---
+++ src/app/pages/plants/main/parent/plant-masonry-grid/plant-masonry-grid.component.spec.ts
@@ -0,0 +1,24 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PlantMasonryGridComponent } from './plant-masonry-grid.component';
+
+describe('PlantMasonryGridComponent', () => {
+  let component: PlantMasonryGridComponent;
+  let fixture: ComponentFixture<PlantMasonryGridComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [PlantMasonryGridComponent],
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PlantMasonryGridComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
