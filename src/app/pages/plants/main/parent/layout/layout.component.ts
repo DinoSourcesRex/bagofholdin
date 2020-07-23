1a6b50cc82a796bfe1b48df98ed96b7c27757f0c
BennieGamali <bengodsan@gmail.com>
2020-07-23 21:53:57 +0200

    moving plant list

--- src/app/pages/plants/main/parent/layout/layout.component.ts
+++ src/app/pages/plants/main/parent/layout/layout.component.ts
@@ -1,10 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-// Services
-import { PlantService } from '@plants/services/plant/plant.service';
-import { Plant } from '@plants/services/models';
-// Third party
-import { NgxMasonryOptions } from 'ngx-masonry';
 
 @Component({
   selector: 'dnd-layout',
@@ -12,16 +6,7 @@ import { NgxMasonryOptions } from 'ngx-masonry';
   styleUrls: ['./layout.component.scss'],
 })
 export class LayoutComponent implements OnInit {
-  allPlants: Observable<Plant[]>;
-
-  masonryOptions: NgxMasonryOptions = {
-    gutter: 10, // The horizontal space between elements
-    animations: {},
-  };
-
-  constructor(service: PlantService) {
-    this.allPlants = service.getAllPlants();
-  }
+  constructor() {}
 
   ngOnInit(): void {}
 }
--- src/app/pages/plants/main/parent/plant-masonry-grid/plant-masonry-grid.component.ts
+++ src/app/pages/plants/main/parent/plant-masonry-grid/plant-masonry-grid.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+// Services
+import { PlantService } from '@plants/services/plant/plant.service';
+import { Plant } from '@plants/services/models';
+// Third party
+import { NgxMasonryOptions } from 'ngx-masonry';
 
 @Component({
   selector: 'dnd-plant-masonry-grid',
   templateUrl: './plant-masonry-grid.component.html',
-  styleUrls: ['./plant-masonry-grid.component.scss']
+  styleUrls: ['./plant-masonry-grid.component.scss'],
 })
 export class PlantMasonryGridComponent implements OnInit {
+  allPlants: Observable<Plant[]>;
 
-  constructor() { }
+  masonryOptions: NgxMasonryOptions = {
+    gutter: 10, // The horizontal space between elements
+    animations: {},
+  };
 
-  ngOnInit(): void {
+  constructor(service: PlantService) {
+    this.allPlants = service.getAllPlants();
   }
 
+  ngOnInit(): void {}
 }
