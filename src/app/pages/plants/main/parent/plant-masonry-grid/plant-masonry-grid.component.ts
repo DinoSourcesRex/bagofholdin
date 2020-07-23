a5f05bfa5595d6b18536215c25e7133f73b7cbb2
BennieGamali <bengodsan@gmail.com>
2020-07-23 21:44:42 +0200

    generating components

---
+++ src/app/pages/plants/main/parent/plant-masonry-grid/plant-masonry-grid.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'dnd-plant-masonry-grid',
+  templateUrl: './plant-masonry-grid.component.html',
+  styleUrls: ['./plant-masonry-grid.component.scss']
+})
+export class PlantMasonryGridComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
